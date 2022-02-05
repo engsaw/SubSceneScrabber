7c24c716271c9e56bf3151f3fe252ac759885589
Sherif Hamad <sherif.hamad@live.com>
2022-02-05 01:10:53 +0100

    Add search on folders and download accordingly

--- src/main/java/Scrabber.java
+++ src/main/java/Scrabber.java
@@ -4,14 +4,19 @@ import io.restassured.response.Response;
 import me.xdrop.fuzzywuzzy.FuzzySearch;
 import net.lingala.zip4j.core.ZipFile;
 import net.lingala.zip4j.exception.ZipException;
-import org.slf4j.Logger;
 
-import java.io.*;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
 import java.nio.file.*;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+
+import static helpers.Helpers.findFiles;
+import static java.lang.Thread.sleep;
 
 
 @Slf4j
@@ -19,55 +24,72 @@ public class Scrabber {
 
     //Define the search Patterns for each page
     private static final Pattern firstPagePattern = Pattern.compile(
-            "/subtitles/+(.+?)"+"\">",
+            "/subtitles/+(.+?)" + "\">",
             Pattern.CASE_INSENSITIVE | Pattern.MULTILINE | Pattern.DOTALL);
 
     private static final Pattern secondPagePattern = Pattern.compile(
-            "/arabic/+(.+?)"+"\">",
+            "/arabic/+(.+?)" + "\">",
             Pattern.CASE_INSENSITIVE | Pattern.MULTILINE | Pattern.DOTALL);
 
     private static final Pattern downloadPagePattern = Pattern.compile(
-            "/subtitles/arabic-text/+(.+?)"+"\"",
+            "/subtitles/arabic-text/+(.+?)" + "\"",
             Pattern.CASE_INSENSITIVE | Pattern.MULTILINE | Pattern.DOTALL);
 
     private static final org.slf4j.Logger log = org.slf4j.LoggerFactory.getLogger(Scrabber.class);
 
 
-    public static void main(String[] args) throws IOException {
+
+    public static void main(String[] args) throws IOException, InterruptedException {
+
+        String movieName;
+        String subsFolder;
+
+        List<String> allMediaLibList = new ArrayList<String>();
+
+        List<String> files = findFiles(Paths.get(args[0]));
+        files.forEach(x -> allMediaLibList.add(x));
+
 
         //Define the List that are used to store links
         List<String> listOfMoviesMatchesSearchLinks = new ArrayList<String>();
         List<String> listOfArabicTranslationLinks = new ArrayList<String>();
-        List<String> downloadLinks ;
-
-        String movieName = args[0];
-        String subsFolder = args[1];
+        List<String> downloadLinks;
 
 
-      searchForMovieAndReturnListOfAvailableCandidates(listOfMoviesMatchesSearchLinks,movieName);
 
-        //Do a fuzzy search to find the best candidate movie
-        int movieIndexInList = FuzzySearch.extractOne(movieName, listOfMoviesMatchesSearchLinks).getIndex();
+        for (String movieNames : allMediaLibList) {
 
 
-        returnListOfArabicTranslations(listOfArabicTranslationLinks, "http://www.subscene.com/subtitles/" + listOfMoviesMatchesSearchLinks.get(movieIndexInList), secondPagePattern);
+            File f = new File(movieNames);
+            movieName = f.getName();
+            subsFolder = f.getParent();
+
+            searchForMovieAndReturnListOfAvailableCandidates(listOfMoviesMatchesSearchLinks, movieName);
+
+            //Do a fuzzy search to find the best candidate movie
+            int movieIndexInList = FuzzySearch.extractOne(movieName, listOfMoviesMatchesSearchLinks).getIndex();
 
 
-        for (int i = 0 ;i<listOfArabicTranslationLinks.size();i++) {
+            listOfArabicTranslationLinks = returnListOfArabicTranslations( "http://www.subscene.com/subtitles/" + listOfMoviesMatchesSearchLinks.get(movieIndexInList), secondPagePattern);
 
-            //visit final page to get download link
-           downloadLinks = returnTheDownloadLink( downloadPagePattern,listOfMoviesMatchesSearchLinks.get(movieIndexInList), listOfArabicTranslationLinks.get(i));
 
-            downloadTheFile(downloadLinks,subsFolder);
+            for (int i = 0; i < listOfArabicTranslationLinks.size(); i++) {
 
-            unZipTheTranslationFile(downloadLinks, subsFolder, subsFolder);
+                //visit final page to get download link
+                downloadLinks = returnTheDownloadLink(downloadPagePattern, listOfMoviesMatchesSearchLinks.get(movieIndexInList), listOfArabicTranslationLinks.get(i));
+
+                downloadTheFile(downloadLinks, subsFolder);
+
+                unZipTheTranslationFile(downloadLinks, subsFolder, subsFolder);
+                sleep (300);
+            }
         }
     }
 
     private static void downloadTheFile(List<String> downloadLinks, String subsFolder) throws IOException {
         //Download File
-        String downLoadLinkFinal="https://subscene.com/subtitles/arabic-text/"+ downloadLinks.get(0);
-        byte[] zipFile  = RestAssured.given()
+        String downLoadLinkFinal = "https://subscene.com/subtitles/arabic-text/" + downloadLinks.get(0);
+        byte[] zipFile = RestAssured.given()
                 .get(downLoadLinkFinal)
                 .andReturn().asByteArray();
 
@@ -76,41 +98,41 @@ public class Scrabber {
 
 
         try {
-            File outputImageFile = new File(subsFolder, downloadLinks.get(0)+".zip"  );
-             outStream = new FileOutputStream(outputImageFile);
+            File outputImageFile = new File(subsFolder, downloadLinks.get(0) + ".zip");
+            outStream = new FileOutputStream(outputImageFile);
 
             outStream.write(zipFile);
 
             outStream.close();
-            String filePath = subsFolder + downloadLinks.get(0)+".zip";
-            log.info("The following file was written in : {}" ,filePath );
-        }catch (Exception e){
+            String filePath = subsFolder + downloadLinks.get(0) + ".zip";
+            //  log.info("The following file was written in : {}" ,filePath );
+        } catch (Exception e) {
 
             log.error(e.getMessage());
-        }
-        finally {
+        } finally {
             //will enhance this later
         }
 
     }
 
-    private static List<String> returnListOfArabicTranslations(List<String> listOfArabicTranslationLinks, String s, Pattern secondPagePattern) {
-        Response response;
+    private static List<String> returnListOfArabicTranslations( String s, Pattern secondPagePattern) {
+        Response myResponse;
+        List <String> listOfArabicTranslationLinks = new ArrayList<>();
 
-        response = RestAssured.given()
+        myResponse = RestAssured.given()
                 .get(s).andReturn();
 
-        Matcher secondPageMatcher = secondPagePattern.matcher(response.asString());
+        Matcher secondPageMatcher = secondPagePattern.matcher(myResponse.asString());
 
 
         while (secondPageMatcher.find()) {
-            listOfArabicTranslationLinks.add(response.asString().substring(secondPageMatcher.start(1),
+            listOfArabicTranslationLinks.add(myResponse.asString().substring(secondPageMatcher.start(1),
                     secondPageMatcher.end(1)));
         }
         return listOfArabicTranslationLinks;
     }
 
-    private static List<String> returnTheDownloadLink(Pattern downloadPagePattern,String movie, String translation) {
+    private static List<String> returnTheDownloadLink(Pattern downloadPagePattern, String movie, String translation) {
         Response response;
 
         String finalLink = "https://subscene.com/subtitles/" + movie + "/arabic/" + translation;
@@ -134,7 +156,7 @@ public class Scrabber {
     private static List<String> searchForMovieAndReturnListOfAvailableCandidates(List<String> listOfMoviesMatchesSearchLinks, String movieName) {
         Response response;
         response = RestAssured.given()
-                .queryParam("query",movieName)
+                .queryParam("query", movieName)
                 .post("https://subscene.com/subtitles/searchbytitle")
                 .andReturn();
         Matcher firstPageMatcher = firstPagePattern.matcher(response.asString());
@@ -151,10 +173,10 @@ public class Scrabber {
         return listOfMoviesMatchesSearchLinks;
     }
 
-    private static void unZipTheTranslationFile(List<String> downloadLinks, String source,String destination ) throws NoSuchFileException, DirectoryNotEmptyException, IOException{
+    private static void unZipTheTranslationFile(List<String> downloadLinks, String source, String destination) throws IOException {
         //extract the file
 
-        String finalZipFileURI = source+ downloadLinks.get(0)+".zip";
+        String finalZipFileURI = source + "/" + downloadLinks.get(0) + ".zip";
         Path path = Paths.get(finalZipFileURI);
 
         try {
@@ -166,9 +188,8 @@ public class Scrabber {
 
         } catch (ZipException e) {
             e.printStackTrace();
-        }
-        finally {
-            log.info("All Zip files are extracted to Subtitle files and deleted" );
+        } finally {
+            log.info(finalZipFileURI);
         }
     }
 
---
+++ src/main/java/helpers/Helpers.java
@@ -0,0 +1,40 @@
+package helpers;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class Helpers {
+
+    public static List<String> findFiles(Path path)
+            throws IOException {
+
+        if (!Files.isDirectory(path)) {
+            throw new IllegalArgumentException("Path must be a directory!");
+        }
+
+        List<String> result;
+
+        try (Stream<Path> walk = Files.walk(path)) {
+            result = walk
+                    .filter(p -> !Files.isDirectory(p))
+                    // this is a path, not string,
+                    // this only test if path end with a certain path
+                    //.filter(p -> p.endsWith(fileExtension))
+                    // convert path to string first
+                    .map(p -> p.toString().toLowerCase())
+                    .filter(f -> f.matches("(.+?)mp4|(.+?)mov|(.+?)wmv|(.+?)avi|(.+?)mkv")
+
+
+                    )
+                    .collect(Collectors.toList());
+
+        }
+
+        return result;
+    }
+
+}
